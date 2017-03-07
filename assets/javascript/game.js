cd66b9e15e0b079f4b3018546779e3d13089af5b
Tsigie Kumssa <tktsigie@yahoo.com>
2017-03-07 17:24:59 -0500

    week-4-game2

---
+++ assets/javascript/game.js
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<title>game.js</title>
+ 
+</head>
+<body>
+<script>
+	
+	
+
+var Wins;
+var Losses;
+var hexagon;
+var dimond;
+var octogon;
+var square
+
+
+var wins = function(){
+
+}
+var losses = function(){
+
+}
+var hexagon = function(){
+	var hexagon = Math.floor(Math.random()*100+1)
+}
+var dimond = function(){
+	var wins = Math.floor(Math.random()*100+1)
+
+}
+var octogon = function(){
+	var wins = Math.floor(Math.random()*100+1)
+
+}
+var square = function(){
+	var wins = Math.floor(Math.random()*100+1)
+
+}
+
+var theNumber = Math.floor(Math.random()*100+1);
+
+ function checkguesses(){
+ 	var myNumber = guess
+ 		alert("you clicked")
+ var wins = 
+
+ 
+
+ function startGame(){
+
+
+
+ }
+
+
+
+
+
+
+
+ function startGame("button")
+
+
+
+
+
+
+
+
+
+
+
+	</script>
+
+</body>
+</html>
